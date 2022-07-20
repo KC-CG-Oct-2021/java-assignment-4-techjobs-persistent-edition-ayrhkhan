e8d445b7afac3a9bad59692e2ccf48b7650be7e8
ayrhkhan <ayrhkhan@gmail.com>
2022-07-20 15:46:15 -0500

    Corrected syntax

--- queries.sql
+++ queries.sql
@@ -4,7 +4,7 @@ In this table (job) I will have columns for id(INT), name(VARCHAR), employer(VAR
 -- Part 2: Test it with SQL
 SELECT name
 FROM employer
-WHERE location = "St.Louis City";
+WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 DROP TABLE job;
@@ -13,6 +13,6 @@ DROP TABLE job;
 -- Part 4: Test it with SQL
 
 SELECT * FROM skill
-INNER JOIN job_skills ON job_skills.skills_id=skill.id
+INNER JOIN job_skills ON job_skills.skills_id = skill.id
 WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
